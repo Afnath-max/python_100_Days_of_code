91f071f8f8e8f3dfb118c684c9cd9627e808d9e5
Afnath-ahamed <it23833548@my.sliit.lk>
2026-01-12 21:22:04 +0530

    project

---
+++ Band Name Generator.py
@@ -0,0 +1,7 @@
+print("Welcome to Band Name Generator")
+city = input("What is the name of the city you grew up in?: ")
+print(city)
+pet = input("What is your pet name?: ")
+print(pet)
+print("Your brand name could be " + city + " " + pet)
+
