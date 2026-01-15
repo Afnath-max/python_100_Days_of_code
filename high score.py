81c2e7bd6e77265360f6f9d08747c0db6d49fc4e
Afnath-ahamed <it23833548@my.sliit.lk>
2026-01-15 17:32:12 +0530

    final update

---
+++ high score.py
@@ -0,0 +1,9 @@
+# Initialize the list
+scores = [10, 20, 30, 40, 50]
+
+maxScore = 0
+for score in scores:
+    if score > maxScore:
+        maxScore = score
+
+print(f"Highest score is: {maxScore}")
